2d46747661e4986625f9d344027745ced5d63c97
İsmail KARGIN <smail>
2024-06-23 18:23:24 +0300

    Taşınmaz adı oluşturma tamamalandı

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import tasinmazAdiOlustuma
 ilceAdi = input("İlçe adını giriniz: ")
 mahalleAdi = input("Mahalle adını giriniz: ")
 adaNo = input("Ada numarasını giriniz: ")
-parselNo = input("Parsek numarasını giriniz")
+parselNo = input("Parsek numarasını giriniz: ")
 
 tasinmazAdiOlustuma.tasinmazAdi(ilceAdi, mahalleAdi, adaNo, parselNo)
--- tasinmazAdiOlustuma.py
+++ tasinmazAdiOlustuma.py
@@ -4,3 +4,10 @@ class tasinmazAdi():
         self.mahalleAdi = mahalleAdi
         self.adaNo = adaNo
         self.parselNo = parselNo
+
+        if adaNo != 0:
+            self.tasinmazAdi = (ilceAdi + " İlçesi, " + mahalleAdi + " Mahallesi, " + str(adaNo) + " Ada " + str(parselNo) + " Parsel")
+        else:
+            self.tasinmazAdi = (ilceAdi + " İlçesi, " + mahalleAdi + " Mahallesi, " + str(parselNo) + " Parsel")
+
+        print(self.tasinmazAdi)
