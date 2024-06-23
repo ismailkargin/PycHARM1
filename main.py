31a5bbf77994b64bcf3a9ad5dae4d7067da23685
İsmail KARGIN <smail>
2024-06-23 18:17:33 +0300

    git status

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("DENEME")
+import tasinmazAdiOlustuma
+
+ilceAdi = input("İlçe adını giriniz: ")
+mahalleAdi = input("Mahalle adını giriniz: ")
+adaNo = input("Ada numarasını giriniz: ")
+parselNo = input("Parsek numarasını giriniz")
+
+tasinmazAdiOlustuma.tasinmazAdi(ilceAdi, mahalleAdi, adaNo, parselNo)
---
+++ tasinmazAdiOlustuma.py
@@ -0,0 +1,6 @@
+class tasinmazAdi():
+    def __init__(self, ilceAdi, mahalleAdi, adaNo, parselNo):
+        self.ilceAdi = ilceAdi
+        self.mahalleAdi = mahalleAdi
+        self.adaNo = adaNo
+        self.parselNo = parselNo
